be2365fcab0d98cbefc906360978e4070f885bcf
Tomas Herma <tomas.herma@letsgood.com>
2015-05-20 08:54:34 +0200

    api url in test file

--- test/index.js
+++ test/index.js
@@ -12,9 +12,7 @@ var TENANT = "synergykit-sample-app",
 describe("SynergyKIT", function() {
     describe("Initialization", function() {
         it("should return intialization credentials", function() {
-            Synergykit.Init(TENANT, KEY, {
-                api: "http://localhost:5078"
-            })
+            Synergykit.Init(TENANT, KEY)
             chai.assert.equal(Synergykit.tenant, TENANT)
             chai.assert.equal(Synergykit.key, KEY)
         })
